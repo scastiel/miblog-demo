88cf42286bd7f2c08c57373bf868fec998f18329
Sébastien Castiel <sebastien.castiel@energiency.com>
2016-11-02 15:17:21 +0100

    update miblog dependency

--- miblog.conf.js
+++ miblog.conf.js
@@ -1,6 +1,6 @@
 var path = require('path');
 module.exports = {
-    baseUrl: 'https://scastiel.github.io/miblog-demo',
+    baseUrl: process.env.BASE_URL || 'https://scastiel.github.io/miblog-demo',
     title: 'Miblog demo',
     description: 'This is a demo blog using [Miblog](https://github.com/scastiel/miblog).',
     footer: 'All content distributed under [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/) license. Powered by [Miblog](https://github.com/scastiel/miblog).',
